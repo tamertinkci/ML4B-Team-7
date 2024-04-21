449f8f687da3ad8d1290e81dead58971130e7096
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-04-21 19:44:08 +0200

    Update grapes_test.py

--- grapes_test.py
+++ grapes_test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Grapes')
+st.title('Grapes Test')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
