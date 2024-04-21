a62ecfca04137da44da4012fa8a2f541b832f4f2
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-04-21 17:55:14 +0200

    Add files via upload

---
+++ uber_pickups.py
@@ -0,0 +1,39 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "232260c0-1c86-4745-9fda-2772a0b94f3e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import streamlit as st\n",
+    "import pandas as pd\n",
+    "import numpy as np\n",
+    "st.title('Uber pickups in NYC')\n",
+    "streamlit run uber_pickups.py"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "ML4B",
+   "language": "python",
+   "name": "ml4b"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
