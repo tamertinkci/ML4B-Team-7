cd9032abba09aaddbc0db1f9285b571b4068cd0c
Stephen <stephen.nkwelle@fau.de>
2024-05-12 09:49:42 +0200

    skeleton for training

---
+++ train/training_loop.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+EPOCHS = 50
+
+
+def load_data(directory):
+    # Check this again
+
+    # code to load data
+    pass
+
+
+def train_step(images):
+    # actual code
+    pass
+
+
+def train(dataset, epochs):
+    # actual code
+    pass
