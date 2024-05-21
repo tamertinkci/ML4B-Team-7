7db2689fe8cd212a0c94e153e523f13b562c07de
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-05-21 13:44:19 +0200

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -14,8 +14,7 @@ print(config['theme']['primaryColor'])
 print(config['theme']['backgroundColor'])
 
 st.title('AppleGenerator')
-st.image("https://oekastatic.orf.at/static/images/site/oeka/20170936/apfel.5650847.jpg", width=100)
-st.video('https://i.gifer.com/7kvq.gif', loop=true, autoplay=true, muted=true)
+st.image('https://i.gifer.com/7kvq.gif', width=100)  
 st.radio("Choose your Apple", ["Normal", "Rot", "Scab"], help='Select an Apple out of 3 choices')
 
 st.write("[![Star](https://img.shields.io/github/stars/tamertinkci/ML4B-Team-7.svg?logo=github&style=social)](https://gitHub.com/tamertinkci/ML4B-Team-7)")
