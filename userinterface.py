38f5c5a261e6a8c12dae1f3a4f6748da217c571d
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-05-21 13:34:18 +0200

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -2,7 +2,6 @@ import streamlit as st
 import toml
 
 st.set_page_config(layout="wide")
-st.video('https://www.youtube.com/watch?v=OGtjFlgGtRQ')
 
 def load_config(file_path):
     with open(file_path, 'r') as file:
@@ -16,6 +15,7 @@ print(config['theme']['backgroundColor'])
 
 st.title('AppleGenerator')
 st.image("https://oekastatic.orf.at/static/images/site/oeka/20170936/apfel.5650847.jpg", width=100)
+st.video('https://www.youtube.com/watch?v=OGtjFlgGtRQ')
 st.radio("Choose your Apple", ["Normal", "Rot", "Scab"], help='Select an Apple out of 3 choices')
 
 st.write("[![Star](https://img.shields.io/github/stars/tamertinkci/ML4B-Team-7.svg?logo=github&style=social)](https://gitHub.com/tamertinkci/ML4B-Team-7)")
