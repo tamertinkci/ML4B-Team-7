673a82d4479dcfc7a214a27085fa54564f4e4498
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-05-21 16:31:08 +0200

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -53,7 +53,7 @@ with tab2:
     st.title("Apple image generator")
     st.image("https://oekastatic.orf.at/static/images/site/oeka/20170936/apfel.5650847.jpg", width=200)
     if st.button("Generate :magic_wand:"):
-        st.image('https://i.gifer.com/7kvq.gif', caption='Wait for it...', width=50, height=50)
+        st.image('https://i.gifer.com/7kvq.gif', caption='Wait for it...', width=1, height=1)
         time.sleep(10)
 
 with tab3:
