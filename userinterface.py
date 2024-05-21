e4f65292980813547bafd3b18c2757b380564f1f
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-05-21 13:36:53 +0200

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -15,7 +15,7 @@ print(config['theme']['backgroundColor'])
 
 st.title('AppleGenerator')
 st.image("https://oekastatic.orf.at/static/images/site/oeka/20170936/apfel.5650847.jpg", width=100)
-st.video('https://www.youtube.com/watch?v=OGtjFlgGtRQ')
+st.video('https://www.youtube.com/watch?v=OGtjFlgGtRQ', loop=true, autoplay=true, muted=true))
 st.radio("Choose your Apple", ["Normal", "Rot", "Scab"], help='Select an Apple out of 3 choices')
 
 st.write("[![Star](https://img.shields.io/github/stars/tamertinkci/ML4B-Team-7.svg?logo=github&style=social)](https://gitHub.com/tamertinkci/ML4B-Team-7)")
