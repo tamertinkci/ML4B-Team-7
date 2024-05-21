b1feb948a2ad9d404a128d5be3e051c0e9a0152d
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-05-21 15:27:19 +0200

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -30,16 +30,17 @@ with tab2:
    st.header("Apple image generator")
    st.image("https://oekastatic.orf.at/static/images/site/oeka/20170936/apfel.5650847.jpg", width=200)
    st.radio("Choose your Apple", ["Normal", "Rot", "Scab"], help='Select an Apple out of 3 choices')
+    
 with tab3:
    st.header("Gallery")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-st.title('AppleGenerator')
+   st.title('AppleGenerator')
 
 with tab4:
    st.header("About Us")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-st.title('AppleGenerator')
-st.write("[![Star](https://img.shields.io/github/stars/tamertinkci/ML4B-Team-7.svg?logo=github&style=social)](https://gitHub.com/tamertinkci/ML4B-Team-7)")
+   st.title('AppleGenerator')
+   st.write("[![Star](https://img.shields.io/github/stars/tamertinkci/ML4B-Team-7.svg?logo=github&style=social)](https://gitHub.com/tamertinkci/ML4B-Team-7)")
    st.image('https://i.gifer.com/7kvq.gif', width=200)
  
 
