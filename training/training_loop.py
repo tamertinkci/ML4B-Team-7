96e2a1ba40e01ed9ae32349eb82056377240b788
Stephen <stephen.nkwelle@fau.de>
2024-06-20 15:54:53 +0200

    adds validation step

--- training/training_loop.py
+++ training/training_loop.py
@@ -23,25 +23,26 @@ def preprocess_image(image, label):
     return image, label
 
 
-# Remove 2nd part (purpose == testing)t if test package is needed
 # change grayscale to rbg if pictures are not colored
 def load_data(image_dir, batch_size, buffer_size, purpose='training'):
     if purpose == 'training':
-        image_dataset = tf.keras.utils.image_dataset_from_directory(
+        image_dataset, val_set = tf.keras.utils.image_dataset_from_directory(
             image_dir,
             image_size=IMAGE_SIZE,
-            color_mode='grayscale',
+            color_mode='rgb',
             batch_size=batch_size,
             shuffle=True,
             seed=42,
-            validation_split=0.2,
-            subset=purpose
+            validation_split=0.15,
+            subset='both'
         )
 
-        image_dataset = image_dataset.map(lambda image, label: preprocess_image(image, label))
+        image_dataset = image_dataset.map(preprocess_image)
+        val_set = val_set.map(preprocess_image)
         image_dataset = image_dataset.shuffle(buffer_size, seed=42).prefetch(tf.data.AUTOTUNE)
 
-        return image_dataset
+        # return validation set too
+        return image_dataset, val_set
 
     elif purpose == 'testing':
         image_dataset = tf.keras.utils.image_dataset_from_directory(
@@ -55,8 +56,10 @@ def load_data(image_dir, batch_size, buffer_size, purpose='training'):
         image_dataset = image_dataset.prefetch(tf.data.AUTOTUNE)
 
         return image_dataset
+    # End of function
 
 
+'''
 # For visual tests
 # Not really necessary otherwise
 def load_and_show(image_dir, buffer_size, purpose='training'):
@@ -72,6 +75,51 @@ def load_and_show(image_dir, buffer_size, purpose='training'):
         plt.imshow(images[0])
         plt.axis('off')
         plt.show()
+    # End of function
+'''
+
+
+# I use 'epoch + 1' because range(x) in from 0 to x-1
+def train(generator, discriminator, train_dataset, val_dataset, noise, epochs):
+    gen_optimizer = loss_calc.optimization()
+    disc_optimizer = loss_calc.optimization()
+    gen_loss_metric = loss_calc.loss_metric()
+    disc_loss_metric = loss_calc.loss_metric()
+    gen_loss = 0
+    disc_loss = 0
+
+    for epoch in range(epochs):
+        start_time = time.time()
+
+        # training
+        for image_batch in train_dataset:
+            gen_loss, disc_loss = _train_step(generator, discriminator,
+                                              gen_optimizer, disc_optimizer, image_batch, noise)
+
+        # Validation
+        for images in val_dataset:
+            _validation_step(generator, discriminator, gen_loss_metric,
+                             disc_loss_metric, images, noise)
+
+        # Print image // Not necessary // Just for fun!!!
+        display.clear_output(wait=True)
+        _generate_and_save_images(
+            generator,
+            noise,
+            epoch + 1
+        )
+
+        # Save checkpoint
+        if epoch % 20 == 0 and epoch != 0:
+            _save_checkpoint(generator, discriminator, gen_optimizer, disc_optimizer)
+
+        # Print progress
+        _print_info(epoch + 1, start_time, gen_loss, disc_loss)
+
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    _generate_and_save_images(generator, noise, epochs)
+    # End of  train function
 
 
 @tf.function  # Improves efficiency
@@ -94,36 +142,19 @@ def _train_step(gen_model, disc_model, gen_optimizer, disc_optimizer, images, no
     return gen_loss, disc_loss
 
 
-# I use 'epoch + 1' because range(x) in from 0 to x-1
-def train(generator, discriminator, dataset, noise, epochs):
-    gen_optimizer = loss_calc.optimization()
-    disc_optimizer = loss_calc.optimization()
-    gen_loss = 0
-    disc_loss = 0
+# @tf.function    # Improves efficiency
+def _validation_step(gen_model, disc_model, gen_loss_metric, disc_loss_metric, images, noise):
+    generated_images = gen_model(noise, training=False)
+    real_output = disc_model(images[0], training=False)
+    fake_output = disc_model(generated_images, training=False)
 
-    for epoch in range(epochs):
-        start_time = time.time()
+    disc_loss = loss_calc.discriminator_loss(real_output, fake_output)
+    gen_loss = loss_calc.generator_loss(fake_output)
 
-        for image_batch in dataset:
-            gen_loss, disc_loss = _train_step(generator, discriminator,
-                                              gen_optimizer, disc_optimizer, image_batch, noise)
-
-        # Print image // Not necessary // Just for fun!!!
-        display.clear_output(wait=True)
-        _generate_and_save_images(
-            generator,
-            noise,
-            epoch + 1
-        )
-
-        if epoch % 20 == 0 and epoch != 0:
-            _save_checkpoint(generator, discriminator, gen_optimizer, disc_optimizer)
-
-        _print_info(epoch + 1, start_time, gen_loss, disc_loss)
-
-    # Generate after the final epoch
-    display.clear_output(wait=True)
-    _generate_and_save_images(generator, noise, epochs)
+    gen_loss_metric.update_state(gen_loss)
+    disc_loss_metric.update_state(disc_loss)
+    # return gen_loss, disc_loss
+    # End of function
 
 
 def _generate_and_save_images(model, noise, epoch):
@@ -134,11 +165,11 @@ def _generate_and_save_images(model, noise, epoch):
 
     for index in range(num_images):
         plt.subplot(4, 4, index + 1)
-        plt.imshow(generated_images[index, :, :, 0] * 127.5 + 127.5, cmap='viridis')
+        plt.imshow(generated_images[index, :, :, 0] * 127.5 + 127.5)
         plt.axis('off')
 
     plt.savefig('./data/train_generated/image_at_epoch_{:03d}.png'.format(epoch))
-    # plt.show()
+    # End of function
 
 
 def _save_checkpoint(generator, discriminator, gen_optimizer, disc_optimizer):
@@ -150,6 +181,7 @@ def _save_checkpoint(generator, discriminator, gen_optimizer, disc_optimizer):
                                      discriminator=discriminator
                                      )
     checkpoint.save(file_prefix=checkpoint_prefix)
+    # End of function
 
 
 def _print_info(epoch, start_time, gen_loss, disc_loss):
@@ -157,4 +189,4 @@ def _print_info(epoch, start_time, gen_loss, disc_loss):
 
     print(f'Epoch: {epoch}, Time: {elapsed_time}s, '
           f'Gen Loss: {gen_loss}, Disc Loss: {disc_loss}')
-
+    # End of function
